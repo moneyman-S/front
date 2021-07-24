f04ab4c125c50182e110fbd573f0255b58292183
Gabriel Nakashima <gabrielmerzarus@gmail.com>
2021-07-24 17:29:43 -0300

    fix:App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Login} from "./pages/Login.tsx"
+import {Login} from "./pages/Login"
 
 function App() {
   return (
