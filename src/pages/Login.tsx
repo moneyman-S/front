83c493769b4fb8944e46b02c7783985e5913c676
Gabriel Nakashima <gabrielmerzarus@gmail.com>
2021-07-24 21:44:01 -0300

    fix: Code Style

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,7 +10,9 @@ export default (props: LoginProps) => {
   return (
     <Container className="d-flex align-content-between justify-content-sm-between flex-column h-100 p-5">
       <Row className="d-flex justify-content-center mt-5 mb-5">
-        <Col md="4" className="ml-6 d-flex justify-content-sm-around" lg="4"><h1>Moneyman</h1></Col>
+        <Col md="4" className="ml-6 d-flex justify-content-sm-around" lg="4">
+          <h1>Moneyman</h1>
+        </Col>
       </Row>
       <Row className="mt-5">
         <Col className="align-self-end">
