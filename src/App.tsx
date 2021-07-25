2f1d72412b4aa8508094573adda8358a259ded81
Gabriel <gabrielvilarmartins@gmail.com>
2021-07-24 21:00:24 -0300

    feat:implement table component

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import Planning from './pages/Planning';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Planning />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
