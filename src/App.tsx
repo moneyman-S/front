5c7efd497cf2fc926bb21adf155b68fb382792ae
Gabriel <gabrielvilarmartins@gmail.com>
2021-07-24 21:04:05 -0300

    remove of deprecated import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Container, Row } from 'react-bootstrap';
