a9114668cca20e43daa24b83d0a96c9cb6bfa45f
Amadeusvh <gabrielvilarmartins@gmail.com>
2021-09-11 15:33:08 -0300

    feat: changing name of site

---
+++ src/SideBarModel.ts
@@ -0,0 +1,19 @@
+import { faHome, faTable, faCog, faUser, IconDefinition, faAddressCard } from "@fortawesome/free-solid-svg-icons";
+import { ModelList } from "./components/SideBar";
+
+const icon = (path: string, iconDef: IconDefinition, description: string) => ({
+  link: path,
+  icon: iconDef,
+  description,
+});
+
+export const navModel: ModelList ={
+  dataSet: [
+    
+    icon("/Home", faHome, "Home"),
+    icon("/", faUser, "User"),
+    icon("#", faTable, "Tables"),
+    icon("#", faCog, "Config"),
+    icon("https://github.com/moneyman-S", faAddressCard, "Contact !")
+  ]
+};
